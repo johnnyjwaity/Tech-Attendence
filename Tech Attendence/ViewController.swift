7d238c54a81abb446b638d48c0f2f907f104a7bd
JohnnyWaity <johnnwaity@gmail.com>
2018-01-16 15:39:47 -0600

    Initial Commit

---
+++ Tech Attendence/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tech Attendence
+//
+//  Created by Johnny Waity on 1/16/18.
+//  Copyright © 2018 Johnny Waity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
